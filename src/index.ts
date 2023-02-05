181d3456ad4041db7eb1736ddff90b87c9a14606
Matias Daloia <matidaloia@gmail.com>
2023-02-05 23:52:28 +0100

    send cid and rid with pageview

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,13 @@ import { EventArgsType, EventType } from "./types/types";
 
 const SESSION_ID_KEY = "fuul.session_id";
 const TRACKING_ID_KEY = "fuul.tracking_id";
+const CAMPAIGN_ID_KEY = "fuul.campaign_id";
+const REFERRER_ID_KEY = "fuul.referrer_id";
 
 const getSessionId = () => localStorage.getItem(SESSION_ID_KEY);
 const getTrackingId = () => localStorage.getItem(TRACKING_ID_KEY);
+const getCampaignId = () => localStorage.getItem(CAMPAIGN_ID_KEY);
+const getReferrerId = () => localStorage.getItem(REFERRER_ID_KEY);
 
 export class Fuul {
   static BASE_API_URL: string = "https://api.fuul.xyz/api/v1";
@@ -29,6 +33,8 @@ export class Fuul {
   ) {
     const session_id = getSessionId();
     const tracking_id = getTrackingId();
+    const campaign_id = getCampaignId();
+    const referrer_id = getReferrerId();
 
     if (!tracking_id) {
       return;
@@ -40,6 +46,8 @@ export class Fuul {
       project_id: projectId,
       event_args: {
         ...args,
+        campaign_id,
+        referrer: referrer_id,
         tracking_id,
       },
     };
@@ -79,6 +87,9 @@ export class Fuul {
     if (!isFuulOrigin) return;
 
     localStorage.setItem(TRACKING_ID_KEY, await this.generateRandomId());
+
+    localStorage.setItem(CAMPAIGN_ID_KEY, queryParams.get("c") ?? "");
+    localStorage.setItem(REFERRER_ID_KEY, queryParams.get("r") ?? "");
   }
 }
 
