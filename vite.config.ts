bc6ce18b7fffbda55781cd1718e16b9881ff80f0
Matias Daloia <matidaloia@gmail.com>
2023-08-18 09:13:52 +0200

    fix: fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,24 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-import { EsLinter, linterPlugin } from 'vite-plugin-linter'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig((configEnv) => ({
+export default defineConfig({
   build: {
     lib: {
-      entry: resolve('src', 'index.ts'),
-      name: 'FuulSdk',
-      fileName: 'fuul-sdk',
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: 'index',
     },
   },
   plugins: [
-    tsConfigPaths(),
-    linterPlugin({
-      include: ['./src}/**/*.{ts,tsx}'],
-      linters: [new EsLinter({ configEnv })],
-    }),
+    tsconfigPaths(),
     dts({
-      include: ['src/'],
+      exclude: ['node_modules'],
+      compilerOptions: {
+        sourceMap: false,
+      },
+      copyDtsFiles: true,
     }),
   ],
-}))
+})
