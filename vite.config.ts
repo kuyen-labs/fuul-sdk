126b8e2bf43cf3b82354a530fd6d960f998f39d0
Matias Daloia <matidaloia@gmail.com>
2023-08-18 09:20:11 +0200

    fix: remove type module

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es', 'cjs'],
+      name: 'Fuul',
       fileName: 'index',
     },
   },
