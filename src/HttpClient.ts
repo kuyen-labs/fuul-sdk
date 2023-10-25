976d34a476726a7aa24ac2c674c840228ae61039
Ale Becher <abecher@gmail.com>
2023-10-25 10:22:07 -0300

    Version

--- src/HttpClient.ts
+++ src/HttpClient.ts
@@ -21,7 +21,7 @@ export class HttpClient {
       ...options,
       headers: {
         Authorization: `Bearer ${options.apiKey}`,
-        'X-Fuul-Sdk-Version': `1.0`,
+        'X-Fuul-Sdk-Version': '3.0.1',
       },
     });
     this.queryParams = options.queryParams ? this.buildQueryParams(options.queryParams) : '';
