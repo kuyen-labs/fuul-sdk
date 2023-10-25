7a16804e8d1c6d195aff70a06469ba7a0444f2dc
Ale Becher <abecher@gmail.com>
2023-10-25 10:25:50 -0300

    fix(version): fix version

--- src/HttpClient.ts
+++ src/HttpClient.ts
@@ -21,7 +21,7 @@ export class HttpClient {
       ...options,
       headers: {
         Authorization: `Bearer ${options.apiKey}`,
-        'X-Fuul-Sdk-Version': '3.0.1',
+        'X-Fuul-Sdk-Version': '3.1.1',
       },
     });
     this.queryParams = options.queryParams ? this.buildQueryParams(options.queryParams) : '';
