2917fd64d42cca265bdbb2c8facb4784e5ab6ba0
Ale Becher <abecher@gmail.com>
2023-11-01 11:58:35 -0300

    Re-add skip ci

--- release.config.cjs
+++ release.config.cjs
@@ -14,7 +14,7 @@ const config = {
       '@semantic-release/git',
       {
         assets: ['dist/**/*.{js,css}', 'package.json'],
-        message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
   ],
