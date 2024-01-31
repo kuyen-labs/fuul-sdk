5ed24f78b01f0f36043a14071e69e49e3430ad44
Matias Daloia <matidaloia@gmail.com>
2024-01-31 09:30:55 +0100

    Export types for backwards compatibility

--- lib/main.ts
+++ lib/main.ts
@@ -1,4 +1,11 @@
-import Fuul from './core';
+import { Conversion } from 'src/modules/conversions/types';
+import { EventArgs, UserMetadata } from 'src/modules/events/types';
 
+import Fuul, { FuulSettings } from './core';
+
+export * from '../src/modules/affiliates/infra/errors';
 export * from './ui';
+
+export type { Conversion, EventArgs, FuulSettings, UserMetadata };
+
 export { Fuul };
