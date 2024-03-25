801615d55aeaa46128907736564a811ca8c6913f
Matias Daloia <matidaloia@gmail.com>
2024-03-25 14:31:08 +0100

    [FT-1197] feat: added getVolumeLeaderboard method

--- src/payouts/PayoutService.ts
+++ src/payouts/PayoutService.ts
@@ -1,5 +1,21 @@
 import { HttpClient } from '../HttpClient';
-import {  GetPayoutsLeaderboardParams, GetPointsLeaderboardParams, GetUserPayoutMovementsParams, GetUserPayoutsByConversionParams, GetUserPointsByConversionParams, GetUserPointsMovementsParams, LeaderboardResponse, PayoutsLeaderboard, PointsLeaderboard, UserPayoutMovementsResponse, UserPayoutsByConversionResponse, UserPointsByConversionResponse, UserPointsMovementsResponse} from '../types/api';
+import {
+  GetPayoutsLeaderboardParams,
+  GetPointsLeaderboardParams,
+  GetUserPayoutMovementsParams,
+  GetUserPayoutsByConversionParams,
+  GetUserPointsByConversionParams,
+  GetUserPointsMovementsParams,
+  GetVolumeLeaderboardParams,
+  LeaderboardResponse,
+  PayoutsLeaderboard,
+  PointsLeaderboard,
+  UserPayoutMovementsResponse,
+  UserPayoutsByConversionResponse,
+  UserPointsByConversionResponse,
+  UserPointsMovementsResponse,
+  VolumeLeaderboard,
+} from '../types/api';
 
 export type PayoutServiceSettings = {
   httpClient: HttpClient;
@@ -28,22 +44,27 @@ export class PayoutService {
   }
 
   public async getUserPayoutsByConversion(params: GetUserPayoutsByConversionParams): Promise<UserPayoutsByConversionResponse> {
-    const results = await this.httpClient.get<UserPayoutsByConversionResponse>(basePath, {...params, type: 'onchain-currency' });
+    const results = await this.httpClient.get<UserPayoutsByConversionResponse>(basePath, { ...params, type: 'onchain-currency' });
     return results.data;
   }
 
   public async getUserPointsByConversion(params: GetUserPointsByConversionParams): Promise<UserPointsByConversionResponse> {
-    const results = await this.httpClient.get<UserPointsByConversionResponse>(basePath, {...params, type: 'point' });
+    const results = await this.httpClient.get<UserPointsByConversionResponse>(basePath, { ...params, type: 'point' });
     return results.data;
   }
 
   public async getUserPayoutMovements(params: GetUserPayoutMovementsParams): Promise<UserPayoutMovementsResponse> {
-    const results = await this.httpClient.get<UserPayoutMovementsResponse>(`${basePath}/movements`, {...params, type: 'onchain-currency' });
+    const results = await this.httpClient.get<UserPayoutMovementsResponse>(`${basePath}/movements`, { ...params, type: 'onchain-currency' });
     return results.data;
   }
 
   public async getUserPointsMovements(params: GetUserPointsMovementsParams): Promise<UserPointsMovementsResponse> {
-    const results = await this.httpClient.get<UserPointsMovementsResponse>(`${basePath}/movements`, {...params, type: 'point' });
+    const results = await this.httpClient.get<UserPointsMovementsResponse>(`${basePath}/movements`, { ...params, type: 'point' });
+    return results.data;
+  }
+
+  public async getVolumeLeaderboard(params: GetVolumeLeaderboardParams): Promise<LeaderboardResponse<VolumeLeaderboard>> {
+    const results = await this.httpClient.get<LeaderboardResponse<VolumeLeaderboard>>(`${basePath}/leaderboard/volume`, params);
     return results.data;
   }
 }
