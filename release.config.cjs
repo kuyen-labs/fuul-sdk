5f63ebf6cfd91620fe7bd251adbd186b11fb9895
Nicolas Garnil <negarnil@gmail.com>
2025-08-06 11:12:09 -0300

    fix: remove dist from semantic-release git assets

--- release.config.cjs
+++ release.config.cjs
@@ -17,7 +17,7 @@ const config = {
     [
       '@semantic-release/git',
       {
-        assets: ['dist/**/*.{js,css}', 'package.json'],
+        assets: ['package.json'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
