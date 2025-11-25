07cb5989a5fd071ddea76d3c5bdffcc4a2b0e553
Nicolas Garnil <negarnil@gmail.com>
2025-11-25 14:44:43 -0300

    fix: add sui_address

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,5 +1,7 @@
 export enum UserIdentifierType {
   EvmAddress = 'evm_address',
   SolanaAddress = 'solana_address',
+  XRPLAddress = 'xrpl_address',
+  SuiAddress = 'sui_address',
   Email = 'email',
 }
