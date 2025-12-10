ec33eb422a8f7b66bb7162001d421ead0a095b96
alejoviola <viola.alejo@outlook.com>
2025-12-10 16:36:04 -0300

    fix: update get user referrer response

--- src/user/types/index.ts
+++ src/user/types/index.ts
@@ -8,4 +8,7 @@ export interface GetUserReferrerParams {
 export interface GetUserReferrerResponse {
   user_identifier: string;
   referrer_identifier: string | null;
+  referrer_name: string | null;
+  referrer_code: string | null;
+  referrer_user_split_percentage: number | null;
 }
